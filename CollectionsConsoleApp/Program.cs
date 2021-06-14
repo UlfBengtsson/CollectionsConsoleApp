bd62a6e6e2ed1482a4d9b4f26104ca980a6fe0bd
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-06-14 11:51:05 +0200

    end of the day with exception och array

--- CollectionsConsoleApp/Program.cs
+++ CollectionsConsoleApp/Program.cs
@@ -8,9 +8,43 @@ namespace CollectionsConsoleApp
         {
             Console.WriteLine("Hello World!");
             //Console.WriteLine("Int Number was: " + GetIntNumberFromUser());
-            Console.WriteLine("Double Number was: " + GetDoubleNumberFromUser());
+            //Console.WriteLine("Double Number was: " + GetDoubleNumberFromUser());
+
+            MultiTable();
             
         }// end of Main method
+
+        static void MultiTable()
+        {
+            // GetLength                    0  1
+            int[,] multi_10_table = new int[10,5];// index for each 0,1,2,3,4,5,6,7,8,9
+
+            for (int y = 0; y < multi_10_table.GetLength(0); y++)
+            {
+
+                for (int x = 0; x < multi_10_table.GetLength(1); x++)
+                {
+                    multi_10_table[y, x] = (y + 1) * (x + 1);
+                }
+
+            }
+
+            for (int y = 0; y < multi_10_table.GetLength(0); y++)
+            {
+
+                for (int x = 0; x < multi_10_table.GetLength(1); x++)
+                {
+                    Console.Write(multi_10_table[y, x] + "\t");
+                }
+                Console.WriteLine();
+            }
+            /*
+            foreach (int number in multi_10_table)
+            {
+                Console.Write(number + "\t");
+            }
+            */
+        }
 
         static int GetIntNumberFromUser()
         {
