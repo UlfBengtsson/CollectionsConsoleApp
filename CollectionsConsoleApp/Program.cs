cb3baf0440edeed9e2cec84230b4d0dcfd915c3e
Ulf Bengtsson <Ulf.Bengtsson.Lexicon@outlook.com>
2021-06-16 11:46:38 +0200

    more examples

--- CollectionsConsoleApp/Program.cs
+++ CollectionsConsoleApp/Program.cs
@@ -1,18 +1,174 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace CollectionsConsoleApp
 {
     class Program
     {
+        /*
+            bool x = "Ulf" == "Ulf";//bad can give false possitives
+            bool y = "Ulf".Equals("Ulf");//correct way
+        */
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             //Console.WriteLine("Int Number was: " + GetIntNumberFromUser());
             //Console.WriteLine("Double Number was: " + GetDoubleNumberFromUser());
+            //MultiTable();
+            //WorkingWithList();
+            //StringBuilderTest();
+            //DictionaryThings();
+            //RandomThings();
 
-            MultiTable();
             
+
+
         }// end of Main method
+
+        /// <summary>
+        /// Primitive values like int/double are sent as copys 
+        /// Objects are sent as a reference (will remember changes inside of them)
+        /// </summary>
+        static void PrimitiveVSObject() 
+        {
+            string test = "test";
+
+            CanIModefieString(test);
+
+            Console.WriteLine(test);
+
+            StringBuilder test2 = new StringBuilder("test2");
+
+            CanIModefieStringBuilder(test2);
+
+            Console.WriteLine(test2);
+        }
+
+        static void CanIModefieStringBuilder(StringBuilder original)
+        {
+            original.Append("mod");
+            //return original;
+        }
+
+        static void CanIModefieString(string original)
+        {
+            original = original + "mod";
+            //return original;
+        }
+
+        static void RandomThings()
+        {
+            Random rng = new Random();//new Random(DateTime.Now.Millisecond);
+            //Random rng2 = new Random(DateTime.Now.Millisecond);
+
+            for (int i = 0; i < 15; i++)
+            {
+                //Console.WriteLine(rng.Next());
+                //Console.WriteLine(rng.Next(10)); // 0 to 9 max is exclusiv
+                Console.WriteLine("Player 1: " + rng.Next(10,20)); // 10 to 19 max is exclusiv but min is inclusiv
+                Console.WriteLine("Player 2: " + rng.Next(10,20)); // 10 to 19 max is exclusiv but min is inclusiv
+            }
+
+        }
+
+        static void DictionaryThings()
+        {
+            Dictionary<int, string> willBankVault = new Dictionary<int, string>();
+
+            willBankVault.Add(666, "The devil");
+            
+            willBankVault.Add(999, "The arch devil");
+
+            foreach (KeyValuePair<int,string> item in willBankVault)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+
+        static void StringThings()
+        {
+            StringBuilder stringBuilderEx = new StringBuilder("test");
+
+            string testString = stringBuilderEx.ToString();
+            string testUpper = "Test";
+
+            Console.WriteLine(testString);
+            Console.WriteLine(testString[2]);
+            Console.WriteLine(testString.ToUpper());
+            Console.WriteLine("is not sensetiv to small big leter? " + testString.Equals(testUpper));
+            Console.WriteLine("is not sensetiv to small big leter (using OrdinalIgnoreCase)? " + testString.Equals(testUpper, StringComparison.OrdinalIgnoreCase));
+        }
+
+        static void StringBuilderTest()
+        {
+            string resultString = "";
+
+            DateTime stringStart = DateTime.Now;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                resultString = resultString + "*";
+            }
+
+            DateTime stringEnd = DateTime.Now;
+
+            TimeSpan stringTime = stringEnd.Subtract(stringStart);
+
+            //------------------------------------------------
+
+            StringBuilder resultSB = new StringBuilder("");
+
+            DateTime stringSBStart = DateTime.Now;
+
+            for (int i = 0; i < 10000; i++)
+            {
+                resultSB = resultSB.Append("*");
+            }
+
+            DateTime stringSBEnd = DateTime.Now;
+
+            TimeSpan sBTime = stringSBEnd.Subtract(stringSBStart);
+
+            Console.WriteLine("String time: " + stringTime);
+            Console.WriteLine("StringBuilder time: " + sBTime);
+
+            //--------------------------------------
+
+            
+        }
+
+        static void WorkingWithList()
+        {
+            List<string> firstNames = new List<string>();//new List<string>(3); start the list with Capacity of 3
+
+            firstNames.Add("Ulf");
+
+            Console.Write("Enter a first name that is not Ulf: ");
+            firstNames.Add(Console.ReadLine());
+
+            PrintList(firstNames, "first names");
+
+            List<string> lastNames = new List<string> { "Andersson", "Bengtsson", "Svensson" };
+
+            //lastNames.Add("Andersson");
+
+            PrintList(lastNames, "last names");
+            
+            Console.WriteLine("Dose first names contain Ulf?: " + firstNames.Contains(firstNames[0]));//firstNames[0]) "Ulf"
+
+            Console.WriteLine("List uses count to say how many are in the list: " + firstNames.Count);
+            Console.WriteLine("List uses Capacity to say how many can be in the list before it expands: " + firstNames.Capacity);
+        }
+
+        static void PrintList(List<string> stringList, string nameOfList)
+        {
+            Console.WriteLine($"-------- {nameOfList} list ----------");
+            foreach (string item in stringList)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine($"-------- end of {nameOfList} list ----------\n");
+        }
 
         static void MultiTable()
         {
